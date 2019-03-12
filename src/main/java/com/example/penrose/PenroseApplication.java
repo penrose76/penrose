ac9468d61b15526588a9097dd4aad2c02e83132c
Marcin Jaromin <jaromin.marcin@gmail.com>
2019-03-12 09:47:04 +0100

    Pierwszy commit

---
+++ src/main/java/com/example/penrose/PenroseApplication.java
@@ -0,0 +1,13 @@
+package com.example.penrose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenroseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenroseApplication.class, args);
+	}
+
+}
